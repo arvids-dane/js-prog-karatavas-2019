e955bf12cc23eecab76b8e4710ea7af8e1bfd221
arvids-dane <arvids.dane@gmail.com>
2019-08-29 16:58:39 +0300

    yolo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,126 @@
 class Karatavas {
-
-}
+    constructor(id) {
+      this.konteiners = document.getElementById(id);
+      if (this.konteiners) {
+        this.konteiners.innerHTML = "Te būs spēle";
+  
+        this.divProgress = document.createElement("div");
+        this.divProgress.setAttribute("class", "progress");
+        this.divProgress.innerHTML = "Progress";
+        this.konteiners.appendChild(this.divProgress);
+  
+        this.divRikjosla = document.createElement("div");
+        this.divRikjosla.setAttribute("class", "rikjosla");
+        let btnJaunaSpele = document.createElement("button");
+        btnJaunaSpele.innerHTML = "Jauna spēle";
+        btnJaunaSpele.onclick = (evt) => {
+            //console.log(evt);
+            this.jaunaSpele();
+        }
+        this.divRikjosla.appendChild(btnJaunaSpele);
+  
+        this.konteiners.appendChild(this.divRikjosla);
+  
+        this.divUzdevums = document.createElement("div");
+        this.divUzdevums.setAttribute("class", "uzdevums");
+        this.divUzdevums.innerHTML = "Uzdevums";
+        this.konteiners.appendChild(this.divUzdevums);
+  
+        this.divBurti = document.createElement("div");
+        this.divBurti.setAttribute("class", "burti");
+        this.divBurti.innerHTML = "Burti";
+        this.konteiners.appendChild(this.divBurti);
+      }
+      this.statuss = Karatavas.STATUSS_NEINICIALIZETS;
+    }
+    novakt() {
+      if (this.statuss === Karatavas.STATUSS_NOVAKTS) return;
+      if (this.konteiners) {
+        this.konteiners.innerHTML = "";
+      }
+      delete this.konteiners;
+      this.statuss = Karatavas.STATUSS_NOVAKTS;
+    }
+    jaunaSpele() {
+      console.log("Sākam jaunu spēli!");
+      this.statuss = Karatavas.STATUSS_SPELE;
+      //console.log(this);
+      this.progress = 0;
+      this.minetieBurti = ""; // Burti, kurus lietotājs ir spiedis
+      this.jaunsUzdevums();
+      this.atjaunotRebusu();
+      console.log("Vēl ir jāsaliek burtu pogas!");
+    }
+    set progress(limenis) {
+      this._progress = limenis;
+      console.log("Jauns progress:", this._progress);
+      if (this.konteiners) {
+        this.divProgress.innerHTML = "Progress: " + this._progress;
+      }
+      if (this._progress == 6) {
+        this.beigas(false);
+      }
+    }
+    get progress() {
+      return this._progress;
+    }
+    jaunsUzdevums() {
+      this.uzdevums = "UZDEVUMS";
+      console.log("Jauns uzdevums: ", this.uzdevums);
+      return this.uzdevums;
+    }
+    atjaunotRebusu() {
+      console.log("Atjaunojam rebusu");
+      let rebuss = "";
+      let uzminets = true;
+      for (var burtaId = 0; burtaId < this.uzdevums.length; burtaId++) {
+        let burts = this.uzdevums.charAt(burtaId);
+        if (this.minetieBurti.indexOf(burts) >= 0) {
+          rebuss += burts;
+        } else {
+          rebuss += "_";
+          uzminets = false;
+        }
+      }
+      if (!this.divUzdevums) return uzminets;
+      this.divUzdevums.innerHTML = rebuss;
+      return uzminets;
+    }
+    minBurtu(burts) {
+      if (this.statuss != Karatavas.STATUSS_SPELE) return;
+      burts = burts.toUpperCase().charAt(0);
+      console.log("Minam burtu:", burts);
+      if (this.minetieBurti.indexOf(burts) >= 0) return; // Burts jau agrāk minēts
+      this.minetieBurti += burts;
+      console.log("Minetie burti:", this.minetieBurti);
+      if (this.uzdevums.indexOf(burts) >= 0) {
+        console.log("Pareizs burts:", burts);
+        if (this.atjaunotRebusu()) {
+          this.beigas(true);
+        }
+      } else {
+        console.log("Nepareizs burts:", burts);
+        this.progress++;
+      }
+    }
+    beigas(uzvara) {
+      if (uzvara) {
+        this.statuss = Karatavas.STATUSS_UZVARA;
+        console.log("Uzvara!");
+      } else {
+        this.statuss = Karatavas.STATUSS_SAGRAVE;
+        if (this.konteiners) {
+          this.divUzdevums.innerHTML = this.uzdevums;
+        }
+        console.log("Sagrāve!");
+      }
+    }
+  }
+  
+  // Statiskās propertijas
+  Karatavas.STATUSS_NEINICIALIZETS = 0; // neinicializēta spēle
+  Karatavas.STATUSS_SPELE = 1; // ir uzdots uzdevums, var minēt
+  Karatavas.STATUSS_UZVARA = 2; // spēle beigusies ar uzvaru
+  Karatavas.STATUSS_SAGRAVE = 3; // spēle beigusies ar zaudējumu
+  Karatavas.STATUSS_NOVAKTS = 9; // spēle ir novākta no ekrāna
+  
